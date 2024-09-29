a9416a36ee1782aa6798c19c4b21a986531166d7
Koya Narendra <narendrakoya134679@gmail.com>
2024-09-29 12:13:13 +0530

    404 Error after deploying to Github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { auth } from './utils/firebaseConfig';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import Layout from './components/Layout/Layout'; 
